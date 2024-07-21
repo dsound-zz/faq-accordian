a366f64562fc6c9ce414f70c28ebf233a6d2f8a4
Demian Sims <demiansims@gmail.com>
2024-07-21 11:05:53 -0400

    added more styling to mobile

--- src/components/Accordian/Accordian.tsx
+++ src/components/Accordian/Accordian.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import "./Accordian.css"
 import open from "../../assets/images/icon-plus.svg"
 import closed from "../../assets/images/icon-minus.svg"
 
@@ -12,25 +13,26 @@ const Accordian = ({ question, answer, index }: AccordianProps) => {
   const [openIndex, setOpenIndex] = useState<number | null>(null)
 
   return (
-    <div>
-      <div>
-        <p>{question}</p>
+    <div className='accordian'>
+      <div className='question-row'>
+        <h3 className='question'>{question}</h3>
         {openIndex === index && (
           <img
-            src={open}
-            alt='open accordian'
+            src={closed}
+            alt='closed accordian'
             onClick={() => setOpenIndex(null)}
           />
         )}
         {openIndex !== index && (
           <img
-            src={closed}
-            alt='closed accordian'
+            src={open}
+            alt='open accordian'
             onClick={() => setOpenIndex(index)}
           />
         )}
-        <p>{openIndex === index && answer}</p>
       </div>
+      <p className='answer'>{openIndex === index && answer}</p>
+      <hr />
     </div>
   )
 }
--- src/components/FaqCard/FaqCard.tsx
+++ src/components/FaqCard/FaqCard.tsx
@@ -5,12 +5,24 @@ import Accordian from "../Accordian/Accordian"
 
 const questionsAndAnswers = [
   {
-    question: "This is a quesiton",
-    answer: "This is an answer",
+    question: "What is Frontend Mentor, and how will it help me?",
+    answer:
+      "Frontend Mentor offers realistic coding challenges to help developers imporve their frontend coding skills with projects in HTML, CSS and JavaScript. It's suitable for all levels and ideal for portfolio building.",
   },
   {
-    question: "This is also another question",
-    answer: "This is also another ans",
+    question: "Is Frontend Mentor free?",
+    answer:
+      "There are 2 levels of Frontend Mentor: free version and paid. The Paid version allows you to access Figma files and upload your work to the community.",
+  },
+  {
+    question: "Can I use Frontend Mentor projects in my portfilio?",
+    answer:
+      "Yes! The whole point of Frontend mentor is to practice a develop your frontend skills and add them to your portfolio.",
+  },
+  {
+    question: "How can I get help if I am stuck on an issue?",
+    answer:
+      "Easy! Just reach out to the community. There are plenty of developers ready to help when the time comes. There is a user forum and Slack.",
   },
 ]
 
